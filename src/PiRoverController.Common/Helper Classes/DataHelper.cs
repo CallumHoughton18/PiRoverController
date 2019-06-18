ab4988bc3909dd6a4337530a38db931fcc4a0068
Callum Houghton <Callum.Houghton@korecgroup.com>
2019-06-18 15:18:29 +0100

    Changed seed data to reflect changes to rasppi server urls.

--- src/PiRoverController.Common/Helper Classes/DataHelper.cs
+++ src/PiRoverController.Common/Helper Classes/DataHelper.cs
@@ -11,14 +11,21 @@ namespace PiRoverController.Common.Helper_Classes
     {
         public static IEnumerable<Setting> GenerateDefaultSettingsData()
         {
-            return new List<Setting>(){new Setting { Id = (int)SettingsIDs.BaseURL, SettingName = "Web Server IP", SettingValue = "http://192.168.0.22:8080/", SettingType = Common.Enums.SettingsType.Wifi },
-                                                   new Setting { Id = (int)SettingsIDs.InitGPIOs, SettingName = "Initialize GPIOs", SettingValue = "/RoverControls/initGPIO", SettingType = Common.Enums.SettingsType.Wifi },
-                                                   new Setting { Id = (int)SettingsIDs.GoForwards, SettingName = "Go Forwards Endpoint", SettingValue = "/RoverControls/forward", SettingType = Common.Enums.SettingsType.Wifi },
-                                                   new Setting { Id = (int)SettingsIDs.GoBackwards, SettingName = "Go Backwards Endpoint", SettingValue = "/RoverControls/backward", SettingType = Common.Enums.SettingsType.Wifi },
-                                                   new Setting { Id = (int)SettingsIDs.GoLeft, SettingName = "Go Left Endpoint", SettingValue = "/RoverControls/left", SettingType = Common.Enums.SettingsType.Wifi },
-                                                   new Setting { Id = (int)SettingsIDs.GoRight, SettingName = "Go Right Endpoint", SettingValue = "/RoverControls/right", SettingType = Common.Enums.SettingsType.Wifi },
-                                                   new Setting { Id = (int)SettingsIDs.StopLeftAndRight, SettingName = "Stop Left&Right Endpoint", SettingValue = "/RoverControls/stopLeftAndRight", SettingType = Common.Enums.SettingsType.Wifi },
-                                                   new Setting { Id = (int)SettingsIDs.StopFowardsAndBackwards, SettingName = "Stop Forwards&Backwards Endpoint", SettingValue = "/RoverControls/stopForwardAndBackward", SettingType = Common.Enums.SettingsType.Wifi } };
+            return new List<Setting>(){new Setting { Id = (int)SettingsIDs.BaseURL, SettingName = "Web Server IP", SettingValue = "http://192.168.0.22:8080", SettingType = SettingsType.Wifi },
+                                                   new Setting { Id = (int)SettingsIDs.InitGPIOs, SettingName = "Initialize GPIOs", SettingValue = "/RoverControls/initGPIO/18/16/11/13", SettingType = SettingsType.Wifi },
+                                                   new Setting { Id = (int)SettingsIDs.GoForwards, SettingName = "Go Forwards Endpoint", SettingValue = "/RoverControls/forward/18/16", SettingType = SettingsType.Wifi },
+                                                   new Setting { Id = (int)SettingsIDs.GoBackwards, SettingName = "Go Backwards Endpoint", SettingValue = "/RoverControls/backward/18/16", SettingType = SettingsType.Wifi },
+                                                   new Setting { Id = (int)SettingsIDs.GoLeft, SettingName = "Go Left Endpoint", SettingValue = "/RoverControls/left/11/13", SettingType = SettingsType.Wifi },
+                                                   new Setting { Id = (int)SettingsIDs.GoRight, SettingName = "Go Right Endpoint", SettingValue = "/RoverControls/right/11/13", SettingType = SettingsType.Wifi },
+                                                   new Setting { Id = (int)SettingsIDs.StopLeftAndRight, SettingName = "Stop Left&Right Endpoint", SettingValue = "/RoverControls/stopLeftAndRight/11/13", SettingType = SettingsType.Wifi },
+                                                   new Setting { Id = (int)SettingsIDs.StopFowardsAndBackwards, SettingName = "Stop Forwards&Backwards Endpoint", SettingValue = "/RoverControls/stopForwardAndBackward/18/16", SettingType = SettingsType.Wifi } };
         }
     }
 }
+
+/*
+GPIO.setup(11, GPIO.OUT) #left
+GPIO.setup(13, GPIO.OUT) #right
+GPIO.setup(16, GPIO.OUT)#reverse
+GPIO.setup(18, GPIO.OUT) #forward
+*/
