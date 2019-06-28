898218d927d64cb1a245fcedf6e5b0dffd3e0e6f
Callum Houghton <callum.houghton13@hotmail.co.uk>
2019-06-29 00:32:35 +0100

    Styled settings page. Applied styling to rover connection indication.

--- src/PiRoverController.Views/Converters/RoverConnectionToColourConverter.cs
+++ src/PiRoverController.Views/Converters/RoverConnectionToColourConverter.cs
@@ -1,4 +1,5 @@
 ﻿using PiRoverController.Common.Enums;
+using PiRoverController.Views.Styles;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -7,20 +8,46 @@ using Xamarin.Forms;
 
 namespace PiRoverController.Views
 {
-    sealed class RoverConnectionToColourConverter : IValueConverter
+    sealed class RoverConnectionToColourConverter : BindableObject, IValueConverter
     {
+        public Color SuccessColor
+        {
+            get { return (Color)base.GetValue(SuccessColorProperty); }
+            set { base.SetValue(SuccessColorProperty, value); }
+        }
+        public static readonly BindableProperty SuccessColorProperty = BindableProperty.Create(nameof(SuccessColor), returnType: typeof(Color), declaringType: typeof(RoverConnectionToColourConverter),
+                                                                                                         defaultValue: Color.Black,
+                                                                                                         defaultBindingMode: BindingMode.TwoWay);
+                                                                                                        
+        public Color FailureColor
+        {
+            get { return (Color)base.GetValue(FailureColorProperty); }
+            set { base.SetValue(FailureColorProperty, value); }
+        }
+        public static readonly BindableProperty FailureColorProperty = BindableProperty.Create(nameof(FailureColor), returnType: typeof(Color), declaringType: typeof(RoverConnectionToColourConverter),
+                                                                                                         defaultValue: Color.Black,
+                                                                                                         defaultBindingMode: BindingMode.TwoWay);
+        public Color InProgressColor
+        {
+            get { return (Color)base.GetValue(InProgressColorProperty); }
+            set { base.SetValue(InProgressColorProperty, value); }
+        }
+        public static readonly BindableProperty InProgressColorProperty = BindableProperty.Create(nameof(InProgressColor), returnType: typeof(Color), declaringType: typeof(RoverConnectionToColourConverter),
+                                                                                                         defaultValue: Color.Black,
+                                                                                                         defaultBindingMode: BindingMode.TwoWay);
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var connectionStatus = (RoverConnection)value;
             switch (connectionStatus)
             {
                 case RoverConnection.Not_Detected:
-                    return Color.Red;
+                    return FailureColor;
                 case RoverConnection.Rover_Detected:
-                    return Color.Green;
+                    return SuccessColor;
             }
 
-            return Color.Orange; //return orange when connecting, and in any other scenarios
+            return InProgressColor; //return orange when connecting, and in any other scenarios
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
--- src/PiRoverController.Views/Views/WifiControlView.xaml.cs
+++ src/PiRoverController.Views/Views/WifiControlView.xaml.cs
@@ -18,6 +18,29 @@ namespace PiRoverController.Views
         public WifiControlView()
         {
             InitializeComponent();
+            //SURPRISE SURPRISE a shitty workaround because UWP support is flaky at best in Xamarin. Visual states do not work as expected.
+            //https://github.com/xamarin/Xamarin.Forms/issues/4910
+            if (Device.RuntimePlatform == Device.UWP)
+            {
+                var imageButtons = new ImageButton[] { ForwardArrow, BackwardArrow, LeftArrow, RightArrow };
+                foreach (var imageButton in imageButtons) VisualStateManager.SetVisualStateGroups(imageButton, new VisualStateGroupList());
+            }
+        }
+
+        private void Animate_Sync_Button(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            if (button != null)
+            {
+                //can be called multiple times through monkey testing, but Xamarin animation handles it without a performance hit or crashing.
+                //if more animation needed it'd be best to create a central animation class to handle this, rather than it being in code behind view.
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await button.RotateTo(360, 500);
+                    await button.RotateTo(0, 0);
+                });
+            }
         }
     }
 }
